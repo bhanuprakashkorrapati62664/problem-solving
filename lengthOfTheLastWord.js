e0916ba466aae41bef186670253cfea250da9674
bhanuprakashkorrapati62664 <bhanuprakashkorrapati04@gmail.com>
2023-11-27 20:19:39 -0500

    longest last word

--- lengthOfTheLastWord.js
+++ lengthOfTheLastWord.js
@@ -1,9 +1,12 @@
-const inp = "luffy is still joyboy";
-const lst = inp.split(" ");
-let res = "";
-for (let i = 0; i < lst.length; i++) {
-  if (lst[i] != "") {
-    res = lst[i];
+const longestOfLastWord = (s) => {
+  const lst = s.split(" ");
+  let res = "";
+  for (let i = 0; i < lst.length; i++) {
+    if (lst[i] != "") {
+      res = lst[i];
+    }
   }
-}
-console.log(res.length);
+  return res.length;
+};
+
+console.log(longestOfLastWord("luffy is still joyboy"));
