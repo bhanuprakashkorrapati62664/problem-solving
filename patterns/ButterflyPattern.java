36da23c5d490ac02a0f9f886b88f491e50e3a172
bhanuprakashkorrapati62664 <74018755+bhanuprakashkorrapati62664@users.noreply.github.com>
2024-01-05 21:22:33 -0500

    pattern programs

---
+++ patterns/ButterflyPattern.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int n = 8;
+        for(int i =1 ; i<= n ;i++){
+            if(i%2 == 0){
+                for(int j = 1; j<= i ; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
