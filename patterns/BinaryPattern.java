3ea0ae394627e1129ded0b89a908900c32c01846
bhanuprakashkorrapati62664 <74018755+bhanuprakashkorrapati62664@users.noreply.github.com>
2024-01-29 22:27:26 -0500

    pattern

--- patterns/BinaryPattern.java
+++ patterns/BinaryPattern.java
@@ -4,9 +4,10 @@ public class BinaryPattern {
         int one = 1;
         for(int i = 1; i <= 5; i++){
             for(int j = 1; j <= i ; j++){
-                if (i%2 != 0 && j%2 != 0) {
+                if (i%2 != 0 && j % 2 != 0 || i % 2 == 0 && j % 2 ==0) {
                     System.out.print(one+" ");
-                }else{
+                }
+                else{
                     System.out.print(zero+" ");
                 }
             }
--- patterns/ButterflyPattern.java
+++ patterns/ButterflyPattern.java
@@ -1,13 +1,23 @@
 public class ButterflyPattern {
     public static void main(String[] args) {
-        int n = 8;
-        for(int i =1 ; i<= n ;i++){
-            if(i%2 == 0){
-                for(int j = 1; j<= i ; j++){
-                    System.out.print("*");
-                }
-                System.out.println();
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
             }
+            for(int k = n; k >= i; k--){
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = n; j >= i; j--){
+                System.out.print("* ");
+            }
+            for(int k = 1; k <= i; k++){
+                System.out.print("# ");
+            }
+            System.out.println();
         }
     }
 }
